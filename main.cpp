c5fb6259e4df9eec284f79396c7d570db6240f8d
Andrey <siventsev@gmail.com>
2026-02-19 19:14:13 +0300

    Update and rename main.cpp to ~.cpp

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include stdlib;
-#include StatsWork.cpp;
-
-StatsWork medcalcul = new StatsWork();
-medcalcul.HandleInp();
-medcalcul.RecalcMediana();
-std::writestr("Mediana:", medcalcul.getMedianaValue());
